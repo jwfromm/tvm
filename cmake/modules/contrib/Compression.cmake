5340aaa39ee532e62bfd1fccebff43be1fca0171
Josh Fromm <jwfromm@uw.edu>
2019-06-03 18:37:17 -0700

    Better compiling

--- cmake/modules/contrib/Compression.cmake
+++ cmake/modules/contrib/Compression.cmake
@@ -1,6 +1,21 @@
 if (USE_COMPRESSION)
+  set (WO_ROOT src/contrib/compression/wolib)
   message(STATUS "Build with waveone compression.")
-  file(GLOB COMPRESSION_CONTRIB_SRC src/contrib/compression/*.cc)
+  include_directories(
+	${WO_ROOT}
+	${WO_ROOT}/thirdparty
+	${WO_ROOT}/wo/framework/default
+	${WO_ROOT}/wo/backend/x86	
+  )
+  file(GLOB COMPRESSION_CONTRIB_SRC 
+  	src/contrib/compression/*.cc
+	${WO_ROOT}/thirdparty/**/*.cc
+	${WO_ROOT}/wo/common/*.cc
+	${WO_ROOT}/wo/aec/*.cc
+	${WO_ROOT}/wo/md5/*.cc
+	${WO_ROOT}/wo/ops/*.cc
+	${WO_ROOT}/wo/thirdparty/**/*.cc
+  )
   list(APPEND RUNTIME_SRCS ${COMPRESSION_CONTRIB_SRC})
-  add_subdirectory(src/contrib/compression/wolib)
+  add_subdirectory(${WO_ROOT})
 endif(USE_COMPRESSION)
